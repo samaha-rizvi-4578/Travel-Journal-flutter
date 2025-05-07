2286dff45811091c311137e87036bfcdba7f896a
SamahaRizvi <k214578@nu.edu.pk>
2025-05-08 00:27:51 +0500

    view journal

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:auth_repo/auth_repo.dart';
 import 'package:auth_ui/auth_ui.dart';
 import '../../../journal/data/journal_repository.dart';
+import '../../../journal/bloc/journal_bloc.dart'; // Import JournalBloc
+import '../../../journal/bloc/journal_event.dart'; // Import JournalEvents
 import './routes/app_router.dart';
 
 class App extends StatelessWidget {
@@ -19,9 +21,17 @@ class App extends StatelessWidget {
         RepositoryProvider<AuthRepository>.value(value: authRepo),
         RepositoryProvider<JournalRepository>.value(value: journalRepo),
       ],
-      child: BlocProvider(
-        create: (context) => AuthBloc(authRepo: authRepo)
-          ..add(AuthUserChanged(authRepo.currentUser)),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(authRepo: authRepo)
+              ..add(AuthUserChanged(authRepo.currentUser)),
+          ),
+          BlocProvider<JournalBloc>(
+            create: (context) => JournalBloc(journalRepository: journalRepo)
+              ..add(LoadJournals()), // Load journals on app start
+          ),
+        ],
         child: MaterialApp.router(
           routerConfig: AppRouter(authRepo).router,
           title: 'Travel Journal',
